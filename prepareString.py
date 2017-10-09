bde6d92d332674cd41682fe8b809dcde363bc8c1
Erik Janežič <erik.janezic@gmail.com>
2017-10-09 16:11:39 +0200

    Cleaned the directory of duplicate files

--- prepareString.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-import nltk
-
-def PrepareString (message):
-    """Prepares a concatenated string of lower case characters from a custom text"""
-    raw = message
-    Words = nltk.word_tokenize(raw)
-    Words = [word.lower() for word in Words if word.isalpha()]
-    return "".join(Words)
