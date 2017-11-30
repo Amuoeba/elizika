c1111c92e1dd7a1e7b8a790dea4da84db09a7869
Erik Janežič <erik.janezic@gmail.com>
2017-11-30 05:41:05 +0100

    added third possible solution

--- SubstitutionCypher Auto Decode/AutoDecode.py
+++ SubstitutionCypher Auto Decode/AutoDecode.py
@@ -173,7 +173,7 @@ def shuffled(seq):
     random.shuffle(seq)
     return seq
 
-def decode_subst(msg,steps=4000,restarts=5000):
+def decode_subst(msg,steps=4000,restarts=25000):
     """Decode a substitution cypher with random restart hillclimbing"""
     msg="".join(allwords(msg)).lower()
     candidates=[hillSearch(encode(msg,key="".join(shuffled(alphabet))),logP3grams,neighboring_msgs,steps) for _ in range(restarts)]
